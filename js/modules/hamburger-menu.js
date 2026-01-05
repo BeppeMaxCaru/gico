330ff3b979c4176506c007bf5d20876b94b2e4a9
Giuseppe Massimiliano Carugno <79905424+BeppeMaxCaru@users.noreply.github.com>
2026-01-05 12:51:21 +0100

    Cleaning files

--- js/modules/hamburger-menu.js
+++ js/modules/hamburger-menu.js
@@ -7,7 +7,114 @@ const menuOptions = {
   ]
 }
 
+const menuStyles = new CSSStyleSheet();
+menuStyles.replaceSync(`
+  :host {
+    display: block;
+    --hamburger-color: var(--color-text);
+  }
+
+  li {
+    padding: 10px;
+    font-family: 'Lato', Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    text-transform: uppercase;
+    font-weight: 400;
+    letter-spacing: 1px;
+    border: 0px;
+    margin: 0px;
+  }
+
+  .menu-container {
+    position: fixed;
+    top: 15px;
+    left: 15px;
+  }
+
+  .hamburger {
+    width: 3rem;
+    aspect-ratio: 1;
+    border: none;
+    background: #222;
+    border-radius: 0.5rem;
+    cursor: pointer;
+    position: relative;
+  }
+
+  .hamburger span {
+    position: absolute;
+    width: 60%;
+    height: 2px;
+    background: white;
+    border-radius: 0.1rem;
+    transform-origin: center;
+    left: 20%;
+    transition: transform  0.25s cubic-bezier(0.4, 0, 0.2, 1), opacity 0.25s ease;
+  }
+  
+  /* POSIZIONAMENTO INIZIALE (3 LINEE) */
+  .hamburger span:nth-child(1) { top: 30%; transform: translateY(-50%); }
+  .hamburger span:nth-child(2) { top: 50%; transform: translateY(-50%); }
+  .hamburger span:nth-child(3) { top: 70%; transform: translateY(-50%); }
+
+  .hamburger.is-active span:nth-child(1) {
+    top: 50%;
+    transform: translateY(-50%) rotate(45deg);
+  }
+
+  .hamburger.is-active span:nth-child(2) {
+    opacity: 0;
+    /*transform: translate(-20px, -50%);*/ /* Scivola via mentre sparisce */
+  }
+
+  .hamburger.is-active span:nth-child(3) {
+    top: 50%;
+    transform: translateY(-50%) rotate(-45deg);
+  }
+
+  /* Dropdown menu */
+  .dropdown {
+    display: none;
+    position: absolute;
+    left: 0;
+    border-radius: 8px;
+    box-shadow: 0 8px 20px rgba(0,0,0,0.15);
+    min-width: 150px;
+    overflow: hidden;
+  }
+
+  .dropdown ul {
+    padding-inline-start: 0%;
+  }
+
+  .dropdown ul li {
+    margin: 10px 0;
+    padding: 5px;
+    background: #222;
+    border-radius: 15px;
+    font-size: 12px;
+  }
+
+  /* Dropdown links */
+  .dropdown a {
+    display: block;
+    padding: 12px;
+    text-decoration: none;
+    color: white;
+  }
+
+  /* Show menu */
+  .dropdown.show {
+    display: block;
+  }
+`);
+
 class HamburgerMenu extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.adoptedStyleSheets = [menuStyles];
+  }
   connectedCallback() {
     this.render();
 
@@ -16,8 +123,8 @@ class HamburgerMenu extends HTMLElement {
   }
 
   _initElements() {
-    this.nav = this.querySelector('[data-js-trigger="menu-toggle"]');
-    this.dropdown = this.querySelector('[data-js-menu="dropdown-options"]');
+    this.nav = this.shadowRoot.querySelector('[data-js-trigger="menu-toggle"]');
+    this.dropdown = this.shadowRoot.querySelector('[data-js-menu="dropdown-options"]');
   }
 
   _addEventListeners() {
@@ -64,7 +171,7 @@ class HamburgerMenu extends HTMLElement {
   };
 
   render() {
-    this.innerHTML = `
+    this.shadowRoot.innerHTML = `
       <div class='menu-container'>
         <button
           class='hamburger'
